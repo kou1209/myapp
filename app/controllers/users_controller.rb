cd65f8b4c1fd6b803ad68084f9a36fd9e4f111ee
nakanokosuke <jaj4fv9ec@icloud.com>
2019-05-25 19:59:14 +0900

    no message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,6 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
-  def index
-    @users = User.paginate(page: params[:page])
-  end
-
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
