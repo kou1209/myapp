3f0f6d57989c6dc6b26193f5c68e77662bb9ca48
nakanokosuke <jaj4fv9ec@icloud.com>
2019-05-27 23:58:26 +0900

    no message

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,2 +1,13 @@
 module MicropostsHelper
+  module CarrierWave
+    module MiniMagick
+      def fix_exif_rotation
+        manipulate! do |img|
+          img.auto_orient
+          img = yield(img) if block_given?
+          img
+        end
+      end
+    end
+  end
 end
