866552e08c8a6676ecda425428be97686f36fe7e
nakanokosuke <jaj4fv9ec@icloud.com>
2019-06-01 23:43:38 +0900

    no message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "#へようこそ"
+      flash[:success] = "コーディネート相談サイトへようこそ"
       redirect_to @user
     else
       render 'new'
