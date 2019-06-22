9201f05a5ee3fed2207c462f8d8bdc81efe64561
nakanokosuke <jaj4fv9ec@icloud.com>
2019-06-23 00:46:05 +0900

    no message

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe Comment, type: :model do
     expect(comment).to be_valid
   end
 
-  it "hoge" do
-    comment = FactoryBot.create(:comment, content: nil)
+  # コメントがなければ無効な状態であること
+  it "is invalid without a comment.content" do
+    comment = FactoryBot.build(:comment, content: nil)
     comment.valid?
     expect(comment.errors[:content]).to include("を入力してください")
   end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Micropost, type: :model do
     micropost = FactoryBot.create(:micropost)
     expect(micropost).to be_valid
   end
+
+  # 投稿文がなければ無効な状態であること
+  it "is invalid without a micropost.content" do
+    micropost = FactoryBot.build(:micropost, content: nil)
+    micropost.valid?
+    expect(micropost.errors[:content]).to include("を入力してください")
+  end
 end
